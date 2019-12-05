7fcc4d7b2e3b18371e2cf617f8cea10563ad5f95
Knut Arild Erstad <knutae@gmail.com>
2019-12-05 07:59:03 +0100

    day 5 part 2

--- 2019/05/solve.py
+++ 2019/05/solve.py
@@ -32,20 +32,42 @@ def run(program, input=[], output=[]):
         if opcode == 99:
             break
         if opcode == 1:
+            # addition
             [a,b,c] = params(3)
             program[c] = getparam(a, 0) + getparam(b, 1)
         elif opcode == 2:
+            # multiplication
             [a,b,c] = params(3)
             program[c] = getparam(a, 0) * getparam(b, 1)
         elif opcode == 3:
+            # input
             [a] = params(1)
             program[a] = input[input_index]
             input_index += 1
         elif opcode == 4:
+            # output
             [a] = params(1)
             r = getparam(a, 0)
             #print(r)
             output.append(r)
+        elif opcode == 5:
+            # jump-if-true
+            [a, b] = params(2)
+            if getparam(a, 0) != 0:
+                ip = getparam(b, 1)
+        elif opcode == 6:
+            # jump-if-false
+            [a, b] = params(2)
+            if getparam(a, 0) == 0:
+                ip = getparam(b, 1)
+        elif opcode == 7:
+            # less than
+            [a, b, c] = params(3)
+            program[c] = 1 if getparam(a, 0) < getparam(b, 1) else 0
+        elif opcode == 8:
+            # equals
+            [a, b, c] = params(3)
+            program[c] = 1 if getparam(a, 0) == getparam(b, 1) else 0
         else:
             assert False
     #print(program[0])
@@ -56,14 +78,39 @@ def run_output(program, input=[]):
     run(program, input, output)
     return output
 
-assert run([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
-assert run([1,0,0,0,99]) == [2,0,0,0,99]
-assert run([2,3,0,3,99]) == [2,3,0,6,99]
-assert run([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-assert run([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
-assert run_output([3,0,4,0,99], [42]) == [42]
-assert run_output([104,42,99], []) == [42]
-assert run([1002,4,3,4,33]) == [1002,4,3,4,99]
+def test():
+    assert run([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
+    assert run([1,0,0,0,99]) == [2,0,0,0,99]
+    assert run([2,3,0,3,99]) == [2,3,0,6,99]
+    assert run([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+    assert run([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+    assert run_output([3,0,4,0,99], [42]) == [42]
+    assert run_output([104,42,99], []) == [42]
+    assert run([1002,4,3,4,33]) == [1002,4,3,4,99]
+    assert run_output([3,9,8,9,10,9,4,9,99,-1,8], [7]) == [0]
+    assert run_output([3,9,8,9,10,9,4,9,99,-1,8], [8]) == [1]
+    assert run_output([3,9,8,9,10,9,4,9,99,-1,8], [9]) == [0]
+    assert run_output([3,9,7,9,10,9,4,9,99,-1,8], [7]) == [1]
+    assert run_output([3,9,7,9,10,9,4,9,99,-1,8], [8]) == [0]
+    assert run_output([3,9,7,9,10,9,4,9,99,-1,8], [9]) == [0]
+    assert run_output([3,3,1108,-1,8,3,4,3,99], [7]) == [0]
+    assert run_output([3,3,1108,-1,8,3,4,3,99], [8]) == [1]
+    assert run_output([3,3,1108,-1,8,3,4,3,99], [9]) == [0]
+    assert run_output([3,3,1107,-1,8,3,4,3,99], [7]) == [1]
+    assert run_output([3,3,1107,-1,8,3,4,3,99], [8]) == [0]
+    assert run_output([3,3,1107,-1,8,3,4,3,99], [9]) == [0]
+    assert run_output([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [0]) == [0]
+    assert run_output([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [-1]) == [1]
+    assert run_output([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [0]) == [0]
+    assert run_output([3,3,1105,-1,9,1101,0,0,12,4,12,99,1], [-42]) == [1]
+    example = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+    assert run_output(example, [7]) == [999]
+    assert run_output(example, [8]) == [1000]
+    assert run_output(example, [9]) == [1001]
+
+test()
 
 def solve(program, input=[1]):
     r = run_output(program, input)
@@ -71,4 +118,5 @@ def solve(program, input=[1]):
     return r
 
 program = [int(x) for x in sys.stdin.read().strip().split(',')]
-solve(program)
+#solve(program)
+solve(program, [5])
