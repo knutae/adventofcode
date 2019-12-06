761f6c1e2ab61b0f10845e4ab81ceba9b3054a3d
Knut Arild Erstad <knutae@gmail.com>
2019-12-06 09:09:45 +0100

    day 6 part 2

--- 2019/06/solve.py
+++ 2019/06/solve.py
@@ -18,6 +18,24 @@ def count_orbits(graph):
         assert v == 'COM'
     return count
 
+def orbit_path(graph, obj):
+    assert obj in graph
+    path = []
+    while obj in graph:
+        obj = graph[obj]
+        path.append(obj)
+    assert path[-1] == 'COM'
+    return path
+
+def shortest_transfer(graph, obj1, obj2):
+    path1 = orbit_path(graph, obj1)
+    path2 = orbit_path(graph, obj2)
+    for i, a in enumerate(path1):
+        for j, b in enumerate(path2):
+            if a == b:
+                return i + j
+    assert False
+
 def test():
     test_input = '''
 COM)B
@@ -33,6 +51,12 @@ J)K
 K)L
 '''
     assert count_orbits(parse_input(test_input)) == 42
+    test2_input = test_input + 'K)YOU\nI)SAN'
+    graph = parse_input(test2_input)
+    assert orbit_path(graph, 'YOU') == ['K', 'J', 'E', 'D', 'C', 'B', 'COM']
+    assert orbit_path(graph, 'SAN') == ['I', 'D', 'C', 'B', 'COM']
+    assert shortest_transfer(graph, 'YOU', 'SAN') == 4
 
 test()
-print(count_orbits(parse_input(sys.stdin.read())))
+#print(count_orbits(parse_input(sys.stdin.read())))
+print(shortest_transfer(parse_input(sys.stdin.read()), 'YOU', 'SAN'))
