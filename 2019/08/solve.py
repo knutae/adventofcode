7af3c41c74f2903baccc9a6336103295229db99e
Knut Arild Erstad <knutae@gmail.com>
2019-12-08 09:40:49 +0100

    day 8 part 2

--- 2019/08/solve.py
+++ 2019/08/solve.py
@@ -2,5 +2,16 @@ import sys
 
 data = sys.stdin.read().strip()
 layers = [data[i:i+25*6] for i in range(0, len(data), 25*6)]
-fewest_zeroes = min((layer for layer in layers), key=lambda x:x.count('0'))
-print(fewest_zeroes.count('1')*fewest_zeroes.count('2'))
+#fewest_zeroes = min((layer for layer in layers), key=lambda x:x.count('0'))
+#print(fewest_zeroes.count('1')*fewest_zeroes.count('2'))
+
+image_data = ['2' for _ in range(25*6)]
+
+for layer in layers:
+    for i in range(len(image_data)):
+        if image_data[i] == '2':
+            image_data[i] = layer[i]
+
+image_data = ''.join(image_data).replace('0', ' ').replace('1', 'X')
+for i in range(0, 25*6, 25):
+    print(image_data[i:i+25])
