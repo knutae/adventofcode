eb3fae48c60003f16c65c26773f9de4522d0beec
Knut Arild Erstad <knutae@gmail.com>
2020-12-07 11:48:13 +0100

    Day 7, part 1

---
+++ 2020/07/solve.py
@@ -0,0 +1,58 @@
+EXAMPLE = '''
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+'''
+
+def parse_n_bags(s):
+    words = s.split(' ')
+    assert len(words) == 4
+    bag = ' '.join(words[1:3])
+    count = int(words[0])
+    if count == 1:
+        assert words[3] == 'bag'
+    else:
+        assert words[3] == 'bags'
+    return bag, count
+
+def parse_line(line):
+    assert line.endswith('.')
+    line = line[:-1]
+    [bag, rest] = line.split(' bags contain ')
+    if rest == 'no other bags':
+        contents = {}
+    else:
+        contents = dict(parse_n_bags(x) for x in rest.split(', '))
+    return bag, contents
+
+def parse(input):
+    lines = map(parse_line, input.strip().split('\n'))
+    return {bag: contents for bag, contents in lines}
+
+def can_contain_shiny_gold(bag, bag_map):
+    contents = bag_map[bag]
+    if len(contents) == 0:
+        return False
+    if 'shiny gold' in contents:
+        return True
+    for key in contents:
+        if can_contain_shiny_gold(key, bag_map):
+            return True
+    return False
+
+def solve1(bag_map):
+    return sum(1 for bag in bag_map if can_contain_shiny_gold(bag, bag_map))
+
+bag_map = parse(EXAMPLE)
+#print(bag_map)
+assert solve1(bag_map) == 4
+
+with open('input') as f:
+    bag_map = parse(f.read())
+print(solve1(bag_map))
