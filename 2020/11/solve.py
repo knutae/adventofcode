e437b852083b3e8cfe1366cbd4393ee5f1429ee0
Knut Arild Erstad <knutae@gmail.com>
2020-12-11 11:37:20 +0100

    Day 11, part 2

--- 2020/11/solve.py
+++ 2020/11/solve.py
@@ -21,11 +21,14 @@ def step(input):
             return '.'
         return lines[y][x]
 
-    def count_adjacent_occupied(x,y):
+    def count_adjacent_occupied(x,y,limit):
         count = 0
         for dx, dy in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
             if get(x+dx, y+dy) == '#':
                 count += 1
+            if count >= limit:
+                # optimization
+                return count
         return count
 
     def new_tile(x,y):
@@ -33,9 +36,9 @@ def step(input):
         if old_tile == '.':
             return '.'
         if old_tile == 'L':
-            return '#' if count_adjacent_occupied(x,y) == 0 else 'L'
+            return '#' if count_adjacent_occupied(x,y,1) == 0 else 'L'
         if old_tile == '#':
-            return 'L' if count_adjacent_occupied(x,y) >= 4 else '#'
+            return 'L' if count_adjacent_occupied(x,y,4) >= 4 else '#'
         assert False
 
     return '\n'.join(
@@ -55,7 +58,65 @@ def solve1(input):
 
 assert solve1(EXAMPLE) == 37
 
+def step2(input):
+    lines = input.split('\n')
+    width = len(lines[0])
+    height = len(lines)
+
+    def get(x,y):
+        if x < 0 or x >= width or y < 0 or y >= height:
+            return 'L'
+        return lines[y][x]
+
+    def can_see_occupied(x,y,dx,dy):
+        while True:
+            x += dx
+            y += dy
+            seen = get(x,y)
+            if seen == 'L':
+                return False
+            if seen == '#':
+                return True
+
+    def count_seen_occupied(x,y,limit):
+        count = 0
+        for dx, dy in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
+            if can_see_occupied(x, y, dx, dy):
+                count += 1
+            if count >= limit:
+                # optimization
+                return count
+        return count
+
+    def new_tile(x,y):
+        old_tile = get(x,y)
+        if old_tile == '.':
+            return '.'
+        if old_tile == 'L':
+            return '#' if count_seen_occupied(x,y,1) == 0 else 'L'
+        if old_tile == '#':
+            return 'L' if count_seen_occupied(x,y,5) >= 5 else '#'
+        assert False
+
+    return '\n'.join(
+        ''.join(new_tile(x, y) for x in range(width))
+        for y in range(height)
+    )
+
+def solve2(input):
+    state = input.strip()
+    #print(state + '\n')
+    while True:
+        new_state = step2(state)
+        #print(new_state + '\n')
+        if new_state == state:
+            return new_state.count('#')
+        state = new_state
+
+assert solve2(EXAMPLE) == 26
+
 with open('input') as f:
     input = f.read().strip()
 
 print(solve1(input))
+print(solve2(input))
