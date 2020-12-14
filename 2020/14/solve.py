dd1c110beed554e2b9d5136a8d67bbe471123901
Knut Arild Erstad <knutae@gmail.com>
2020-12-14 12:33:58 +0100

    Day 14, part 2

--- 2020/14/solve.py
+++ 2020/14/solve.py
@@ -45,7 +45,52 @@ def solve1(input):
 
 assert solve1(EXAMPLE) == 165
 
+def apply_address_mask(mask, n):
+    result = []
+    for i, m in enumerate(reversed(mask)):
+        if m == '0':
+            result.append(str(1 & (n >> i)))
+        else:
+            assert m in '1X'
+            result.append(m)
+    result.reverse()
+    return ''.join(result)
+
+assert apply_address_mask('000000000000000000000000000000X1001X', 42) == '000000000000000000000000000000X1101X'
+
+def generate_addresses(mask):
+    i = mask.find('X')
+    if i == -1:
+        yield int(mask, 2)
+    else:
+        for c in '01':
+            for address in generate_addresses(mask[:i] + c + mask[i+1:]):
+                yield address
+
+assert list(generate_addresses('000000000000000000000000000000X1101X')) == [26, 27, 58, 59]
+
+EXAMPLE2 = '''
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+'''
+
+def solve2(input):
+    mask = None
+    mem = dict()
+    for a, b in parse(input):
+        if a == 'mask':
+            mask = b
+        else:
+            for address in generate_addresses(apply_address_mask(mask, a)):
+                mem[address] = b
+    return sum(mem.values())
+
+assert solve2(EXAMPLE2) == 208
+
 with open('input') as f:
     input = f.read()
 
 print(solve1(input))
+print(solve2(input))
