57b8dbe385eadfac04f0d7220495a1b34629c02a
Knut Arild Erstad <knutae@gmail.com>
2020-12-18 12:33:00 +0100

    Day 18, part 2

--- 2020/18/solve.py
+++ 2020/18/solve.py
@@ -54,20 +54,49 @@ def eval_expression(expr):
             assert False
     return result
 
-def eval_line(line):
+def eval1(line):
     return eval_expression(parse_expression(line))
 
-assert eval_line('1 + 2 * 3 + 4 * 5 + 6') == 71
-assert eval_line('1 + (2 * 3) + (4 * (5 + 6))') == 51
-assert eval_line('2 * 3 + (4 * 5)') == 26
-assert eval_line('5 + (8 * 3 + 9 + 3 * 4 * 3)') == 437
-assert eval_line('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 12240
-assert eval_line('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 13632
+assert eval1('1 + 2 * 3 + 4 * 5 + 6') == 71
+assert eval1('1 + (2 * 3) + (4 * (5 + 6))') == 51
+assert eval1('2 * 3 + (4 * 5)') == 26
+assert eval1('5 + (8 * 3 + 9 + 3 * 4 * 3)') == 437
+assert eval1('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 12240
+assert eval1('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 13632
 
 def solve1(input):
-    return sum(eval_line(line) for line in input.strip().split('\n'))
+    return sum(eval1(line) for line in input.strip().split('\n'))
+
+def force_precedence(expr):
+    if isinstance(expr, int):
+        return expr
+    assert isinstance(expr, list)
+    expr = [force_precedence(x) if isinstance(x, list) else x for x in expr]
+    while len(expr) > 3 and '+' in expr:
+        i = expr.index('+')
+        new_subexpr = expr[i-1:i+2]
+        assert len(new_subexpr) == 3 and new_subexpr[1] == '+'
+        expr = expr[:i-1] + [new_subexpr] + expr[i+2:]
+    assert len(expr) % 2 == 1
+    return expr
+
+def eval2(line):
+    expr = parse_expression(line)
+    expr = force_precedence(expr)
+    return eval_expression(expr)
+
+assert eval2('1 + 2 * 3 + 4 * 5 + 6') == 231
+assert eval2('1 + (2 * 3) + (4 * (5 + 6))') == 51
+assert eval2('2 * 3 + (4 * 5)') == 46
+assert eval2('5 + (8 * 3 + 9 + 3 * 4 * 3)') == 1445
+assert eval2('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 669060
+assert eval2('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 23340
+
+def solve2(input):
+    return sum(eval2(line) for line in input.strip().split('\n'))
 
 with open('input') as f:
     input = f.read()
 
 print(solve1(input))
+print(solve2(input))
