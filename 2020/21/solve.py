93d76b3c0a1ebad36467701aba80cb4858bc649c
Knut Arild Erstad <knutae@gmail.com>
2020-12-21 14:31:21 +0100

    Day 21, part 2

--- 2020/21/solve.py
+++ 2020/21/solve.py
@@ -34,6 +34,34 @@ def solve1(input):
 
 assert solve1(EXAMPLE) == 5
 
+def solve2(input):
+    foods = parse(input)
+    all_allergens = set.union(*(set(allergens) for _, allergens in foods))
+    all_ingredients = set.union(*(set(ingredients) for ingredients, _ in foods))
+    ingredients_per_allergen = {
+        allergen: ingredients_possibly_containing_allergen(foods, allergen)
+        for allergen in all_allergens
+    }
+    while any(len(ingredients) > 1 for ingredients in ingredients_per_allergen.values()):
+        removals = 0
+        for allergen, ingredients in ingredients_per_allergen.items():
+            if len(ingredients) == 1:
+                # eliminate this ingredient from all other values
+                ingredient = next(iter(ingredients))
+                #print(f'Eliminating ingredient {ingredient} from others')
+                for other_ingredients in ingredients_per_allergen.values():
+                    if len(other_ingredients) > 1 and ingredient in other_ingredients:
+                        other_ingredients.remove(ingredient)
+                        removals += 1
+        assert removals > 0
+    #print(ingredients_per_allergen)
+    result = [next(iter(ingredients_per_allergen[a])) for a in sorted(ingredients_per_allergen.keys())]
+    #print(result)
+    return ','.join(result)
+
+assert solve2(EXAMPLE) == 'mxmxvkd,sqjhc,fvjkl'
+
 with open('input') as f:
     input = f.read()
 print(solve1(input))
+print(solve2(input))
