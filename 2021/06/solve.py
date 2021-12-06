4b353aa517539734ea5f3016ec060c82e403136d
Knut Arild Erstad <knutae@gmail.com>
2021-12-06 11:59:42 +0100

    Day 6, slightly shorter

--- 2021/06/solve.py
+++ 2021/06/solve.py
@@ -4,14 +4,12 @@ def solve(ages, num_days):
         counts_per_age[age] += 1
     for _ in range(num_days):
         respawns = counts_per_age[0]
-        counts_per_age = counts_per_age[1:] + [0]
+        counts_per_age = counts_per_age[1:] + [respawns]
         counts_per_age[6] += respawns
-        counts_per_age[8] += respawns
     return sum(counts_per_age)
 
 assert solve([3,4,3,1,2], 18) == 26
 assert solve([3,4,3,1,2], 80) == 5934
-
 assert solve([3,4,3,1,2], 256) == 26984457539
 
 with open('input') as f:
