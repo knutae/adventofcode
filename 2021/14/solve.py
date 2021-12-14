019be1707d386228218d87a92a45325babab1b66
Knut Arild Erstad <knutae@gmail.com>
2021-12-14 14:41:51 +0100

    Day 14, part 2

--- 2021/14/solve.py
+++ 2021/14/solve.py
@@ -60,7 +60,33 @@ test()
 
 assert solve1(EXAMPLE) == 1588
 
+def step_counts(counts, rules):
+    new_counts = Counter()
+    for pair, count in counts.items():
+        a,b = pair
+        c = rules[pair]
+        new_counts[a+c] += count
+        new_counts[c+b] += count
+    return new_counts
+
+def solve2(data, steps=40):
+    polymer, rules = parse(data)
+    counts = Counter()
+    for a,b in zip(polymer, polymer[1:]):
+        counts[a+b] += 1
+    for i in range(steps):
+        counts = step_counts(counts, rules)
+    letter_counts = Counter()
+    for pair, count in counts.items():
+        letter_counts[pair[0]] += count
+    letter_counts[polymer[-1]] += 1 # fix last letter count
+    return max(letter_counts.values()) - min(letter_counts.values())
+
+assert solve2(EXAMPLE, 10) == 1588
+assert solve2(EXAMPLE) == 2188189693529
+
 with open('input') as f:
     puzzle_input = f.read()
 
 print(solve1(puzzle_input))
+print(solve2(puzzle_input))
