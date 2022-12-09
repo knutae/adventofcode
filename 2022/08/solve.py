51f0169e9f231e75e8843295921702433160d89c
Knut Arild Erstad <knutae@gmail.com>
2022-12-09 01:35:45 +0100

    Day 8, part 2

--- 2022/08/solve.py
+++ 2022/08/solve.py
@@ -22,9 +22,41 @@ def is_visible(grid, x, y):
 def solve1(grid):
     return sum(1 for y in range(len(grid)) for x in range(len(grid[0])) if is_visible(grid, x, y))
 
-assert solve1(parse(TEST_INPUT)) == 21
+def viewing_distance(grid, x, y, dx, dy):
+    h = grid[y][x]
+    tx = x + dx
+    ty = y + dy
+    distance = 0
+    while tx >= 0 and tx < len(grid[0]) and ty >= 0 and ty < len(grid):
+        distance += 1
+        if grid[ty][tx] >= h:
+            break
+        tx += dx
+        ty += dy
+    return distance
+
+def scenic_score(grid, x, y):
+    return (
+        viewing_distance(grid, x, y, -1, 0) *
+        viewing_distance(grid, x, y, 1, 0) *
+        viewing_distance(grid, x, y, 0, -1) *
+        viewing_distance(grid, x, y, 0, 1)
+    )
+
+def solve2(grid):
+    return max(scenic_score(grid, x, y) for y in range(len(grid)) for x in range(len(grid[0])))
+
+test_input = parse(TEST_INPUT)
+assert solve1(test_input) == 21
+assert viewing_distance(test_input, 2, 1, 0, -1) == 1
+assert viewing_distance(test_input, 2, 1, -1, 0) == 1
+assert viewing_distance(test_input, 2, 1, 1, 0) == 2
+assert viewing_distance(test_input, 2, 1, 0, 1) == 2
+assert scenic_score(test_input, 2, 1) == 4
+assert solve2(test_input) == 8
 
 with open('input') as f:
     input = f.read()
 
 print(solve1(parse(input)))
+print(solve2(parse(input)))
