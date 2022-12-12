59d3cb1bab8bb7c8efcbd94e4769db715b6d570b
Knut Arild Erstad <knutae@gmail.com>
2022-12-12 11:34:46 +0100

    Day 12, part 2

--- 2022/12/solve.py
+++ 2022/12/solve.py
@@ -51,9 +51,33 @@ def solve1(input):
         coords = new_coords
     return distances[end]
 
+def solve2(input):
+    heights, _, end = parse(input)
+    distances = {c: 0 for c in heights.keys() if heights[c] == 0}
+    coords = set(distances.keys())
+    while end not in distances:
+        new_coords = set()
+        for x, y in coords:
+            height = heights[(x, y)]
+            distance = distances[(x, y)]
+            for nx, ny in directions(x, y):
+                if (nx, ny) not in heights or heights[(nx, ny)] > height + 1:
+                    # no path
+                    continue
+                if (nx, ny) in distances:
+                    # already visited
+                    assert distances[(nx, ny)] <= distance + 1
+                    continue
+                distances[(nx, ny)] = distance + 1
+                new_coords.add((nx, ny))
+        coords = new_coords
+    return distances[end]
+
 assert solve1(EXAMPLE) == 31
+assert solve2(EXAMPLE) == 29
 
 with open('input') as f:
     input = f.read()
 
 print(solve1(input))
+print(solve2(input))
