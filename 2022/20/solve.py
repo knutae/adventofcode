e271370d6d50f7d9f3a45c1ea04784e431914f0a
Knut Arild Erstad <knutae@gmail.com>
2022-12-20 12:03:58 +0100

    Day 20, part 1

---
+++ 2022/20/solve.py
@@ -0,0 +1,37 @@
+EXAMPLE = '''
+1
+2
+-3
+3
+-2
+0
+4
+'''
+
+def parse(input):
+    return [int(x) for x in input.strip().split('\n')]
+
+def move(current_numbers, original_index, n):
+    current_index = current_numbers.index((original_index, n))
+    del current_numbers[current_index]
+    new_index = (current_index + n) % len(current_numbers)
+    current_numbers.insert(new_index, (original_index, n))
+
+def solve1(input):
+    numbers = parse(input)
+    current_numbers = list(enumerate(numbers))
+    for i, n in enumerate(numbers):
+        move(current_numbers, i, n)
+    zero_index = [i for i, num in enumerate(current_numbers) if num[1] == 0]
+    assert len(zero_index) == 1
+    zero_index = zero_index[0]
+    results = [current_numbers[(zero_index + x) % len(current_numbers)][1] for x in (1000, 2000, 3000)]
+    #print(results)
+    return sum(results)
+
+assert solve1(EXAMPLE) == 3
+
+with open('input') as f:
+    input = f.read()
+
+print(solve1(input))
