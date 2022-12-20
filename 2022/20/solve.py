9d8c9ac7bc082c2038209c114f175a5966f60dd3
Knut Arild Erstad <knutae@gmail.com>
2022-12-20 12:07:36 +0100

    Day 20, part 2

--- 2022/20/solve.py
+++ 2022/20/solve.py
@@ -29,9 +29,25 @@ def solve1(input):
     #print(results)
     return sum(results)
 
+def solve2(input):
+    numbers = parse(input)
+    numbers = [n*811589153 for n in numbers]
+    current_numbers = list(enumerate(numbers))
+    for _ in range(10):
+        for i, n in enumerate(numbers):
+            move(current_numbers, i, n)
+    zero_index = [i for i, num in enumerate(current_numbers) if num[1] == 0]
+    assert len(zero_index) == 1
+    zero_index = zero_index[0]
+    results = [current_numbers[(zero_index + x) % len(current_numbers)][1] for x in (1000, 2000, 3000)]
+    #print(results)
+    return sum(results)
+
 assert solve1(EXAMPLE) == 3
+assert solve2(EXAMPLE) == 1623178306
 
 with open('input') as f:
     input = f.read()
 
 print(solve1(input))
+print(solve2(input))
