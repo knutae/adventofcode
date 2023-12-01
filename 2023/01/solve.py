26e056cf6215f659f744ac31f325b4e260e24a03
Knut Arild Erstad <knutae@gmail.com>
2023-12-01 17:08:25 +0100

    Day 1, part 2

--- 2023/01/solve.py
+++ 2023/01/solve.py
@@ -5,6 +5,17 @@ a1b2c3d4e5f
 treb7uchet
 """
 
+EXAMPLE2 = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
+
+
 def solve1(data):
     lines = data.strip().split("\n")
     r = 0
@@ -13,9 +24,39 @@ def solve1(data):
         r += int(digits[0] + digits[-1])
     return r
 
-assert solve1(EXAMPLE) == 142
 
-with open('input') as f:
+DIGITS = {
+    word: str(i + 1)
+    for i, word in enumerate(
+        ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    )
+} | {str(i): str(i) for i in range(1, 10)}
+
+
+def solve2(data):
+    lines = data.strip().split("\n")
+    r = 0
+    for line in lines:
+        first, last = (len(line), None), (-1, None)
+        for word, i in DIGITS.items():
+            lpos = line.find(word)
+            if lpos != -1 and lpos < first[0]:
+                assert lpos != first[0]
+                first = lpos, i
+            rpos = line.rfind(word)
+            if rpos != -1 and rpos > last[0]:
+                assert rpos != last[0]
+                last = rpos, i
+        #print(line, first, last)
+        r += int(first[1] + last[1])
+    return r
+
+
+assert solve1(EXAMPLE) == 142
+assert solve2(EXAMPLE2) == 281
+
+with open("input") as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
