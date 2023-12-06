e3c57fd3cbe11f8b62f913d096561d20a25f1cfd
Knut Arild Erstad <knutae@gmail.com>
2023-12-06 11:23:35 +0100

    Day 6, part 1

---
+++ 2023/06/solve.py
@@ -0,0 +1,32 @@
+import math
+
+EXAMPLE = '''
+Time:      7  15   30
+Distance:  9  40  200
+'''
+
+def parse(data):
+    times, distances = data.strip().split('\n')
+    times = [int(x) for x in times.split()[1:]]
+    distances = [int(x) for x in distances.split()[1:]]
+    return times, distances
+
+def solve1(data):
+    times, distances = parse(data)
+    r = []
+    for time, distance in zip(times, distances):
+        ways_to_solve = 0
+        for button_time in range(1, time):
+            travel_time = time - button_time
+            dist = button_time * travel_time
+            if dist > distance:
+                ways_to_solve += 1
+        r.append(ways_to_solve)
+    return math.prod(r)
+
+assert solve1(EXAMPLE) == 288
+
+with open('input') as f:
+    data = f.read()
+
+print(solve1(data))
