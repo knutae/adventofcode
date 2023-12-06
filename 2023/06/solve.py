e8e126f9d060f998160dbe8375db6467b4f90422
Knut Arild Erstad <knutae@gmail.com>
2023-12-06 11:29:11 +0100

    Day 6, part 2 (brute force)

--- 2023/06/solve.py
+++ 2023/06/solve.py
@@ -26,7 +26,22 @@ def solve1(data):
 
 assert solve1(EXAMPLE) == 288
 
+def solve2(data):
+    times, distances = parse(data)
+    time = int(''.join(map(str, times)))
+    distance = int(''.join(map(str, distances)))
+    ways_to_solve = 0
+    for button_time in range(1, time):
+        travel_time = time - button_time
+        dist = button_time * travel_time
+        if dist > distance:
+            ways_to_solve += 1
+    return ways_to_solve
+
+assert solve2(EXAMPLE) == 71503
+
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
