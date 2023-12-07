a3986d88f20060eadf393ca8f187bcfb968a7c32
Knut Arild Erstad <knutae@gmail.com>
2023-12-07 12:00:28 +0100

    Day 7, part 2

--- 2023/07/solve.py
+++ 2023/07/solve.py
@@ -53,9 +53,57 @@ def solve1(data):
         #print((i + 1), hand, bid, (i + 1) * bid)
     return result
 
+def hand_type_with_jokers(hand):
+    if 'J' not in hand:
+        return hand_type(hand)
+    joker_count = hand.count('J')
+    counts = list(sorted({c: hand.count(c) for c in hand if c != 'J'}.values()))
+    if len(counts) <= 1:
+        # five of a kind
+        return 6
+    if len(counts) == 2:
+        # with two kinds of non-jokers, we can always do four of a kind or full house
+        if 1 in counts:
+            # four of a kind
+            return 5
+        else:
+            # full house
+            return 4
+    if len(counts) == 3:
+        # as seen by asserts below, this will always give three of a kind
+        assert max(counts) <= 2
+        assert joker_count <= 2
+        if 2 in counts:
+            assert counts == [1, 1, 2] and joker_count == 1
+        else:
+            assert counts == [1, 1, 1] and joker_count == 2
+        return 3
+    if len(counts) == 4:
+        assert counts == [1, 1, 1, 1] and joker_count == 1
+        # one pair
+        return 1
+    assert False
+
+JOKER_CARDS = 'J23456789TQKA'
+
+def hand_key_with_jokers(hand):
+    return hand_type_with_jokers(hand), tuple(JOKER_CARDS.index(c) for c in hand)
+
+def solve2(data):
+    hands_and_bids = parse(data)
+    hands_and_bids.sort(key = lambda x: hand_key_with_jokers(x[0]))
+    result = 0
+    for i, item in enumerate(hands_and_bids):
+        _, bid = item
+        result += (i + 1) * bid
+        #print((i + 1), hand, bid, (i + 1) * bid)
+    return result
+
 assert solve1(EXAMPLE) == 6440
+assert solve2(EXAMPLE) == 5905
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
