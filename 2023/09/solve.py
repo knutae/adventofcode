d9dc4c8a40b57de96c2bef34ded3fa05d0069477
Knut Arild Erstad <knutae@gmail.com>
2023-12-09 10:55:22 +0100

    Day 9, part 2

--- 2023/09/solve.py
+++ 2023/09/solve.py
@@ -21,9 +21,22 @@ def solve1(data):
     lines = parse(data)
     return sum(predict_next(line) for line in lines)
 
+def predict_previous(sequence):
+    if all(x == 0 for x in sequence):
+        return 0
+    diffs = [b-a for a, b in zip(sequence, sequence[1:])]
+    #print(sequence, diffs)
+    return sequence[0] - predict_previous(diffs)
+
+def solve2(data):
+    lines = parse(data)
+    return sum(predict_previous(line) for line in lines)
+
 assert solve1(EXAMPLE) == 114
+assert solve2(EXAMPLE) == 2
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
