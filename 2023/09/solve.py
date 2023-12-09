27ea982aa09c7d06461de736c3e4e0bf9fedb96d
Knut Arild Erstad <knutae@gmail.com>
2023-12-09 10:53:03 +0100

    Day 9, part 1

---
+++ 2023/09/solve.py
@@ -0,0 +1,29 @@
+EXAMPLE = '''
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+'''
+
+def parse_line(line):
+    return [int(x) for x in line.split()]
+
+def parse(data):
+    return [parse_line(line) for line in data.strip().split('\n')]
+
+def predict_next(sequence):
+    if all(x == 0 for x in sequence):
+        return 0
+    diffs = [b-a for a, b in zip(sequence, sequence[1:])]
+    #print(sequence, diffs)
+    return sequence[-1] + predict_next(diffs)
+
+def solve1(data):
+    lines = parse(data)
+    return sum(predict_next(line) for line in lines)
+
+assert solve1(EXAMPLE) == 114
+
+with open('input') as f:
+    data = f.read()
+
+print(solve1(data))
