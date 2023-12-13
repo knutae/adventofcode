453da61891e15ad0889440042a1a7fac3c195d34
Knut Arild Erstad <knutae@gmail.com>
2023-12-13 01:15:44 +0100

    Day 12, part 2 (actually not slow)

--- 2023/12/solve.py
+++ 2023/12/solve.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 EXAMPLE = '''
 ???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -9,7 +11,7 @@ EXAMPLE = '''
 
 def parse_row(line):
     record, counts = line.split(' ')
-    counts = [int(x) for x in counts.split(',')]
+    counts = tuple(int(x) for x in counts.split(','))
     return record, counts
 
 def parse(data):
@@ -20,41 +22,52 @@ def simplify_record(record):
 
 assert simplify_record('????.#...#...') == '????.#.#'
 
-def generate_all(record):
-    index = record.find('?')
-    if index < 0:
-        yield record
+def string_matches(record, s):
+    return all(a == '?' or a == b for a,b in zip(record, s))
+
+@cache
+def count_matching_strings(record, counts):
+    length = len(record)
+    if len(counts) == 0:
+        return 1
+    first_count, *rest_counts = counts
+    rest_counts = tuple(rest_counts)
+    result = 0
+    if len(rest_counts) == 0:
+        for i in range(length - first_count + 1):
+            s = '.' * i + '#' * first_count + '.' * (length - first_count - i)
+            #assert len(s) == length
+            if string_matches(record, s):
+                result += 1
     else:
-        for r in '.#':
-            replaced = record[:index] + r + record[index+1:]
-            for x in generate_all(replaced):
-                yield x
-
-assert list(generate_all('.#')) == ['.#']
-assert list(generate_all('?')) == ['.', '#']
-assert list(generate_all('?.?')) == ['...', '..#', '#..', '#.#']
-
-def count_springs(record):
-    assert '?' not in record
-    return [len(x) for x in record.split('.') if x]
-
-assert count_springs('.#..#...###.') == [1,1,3]
-
-def count_matching_combinations(record, counts):
-    r = sum(1 for r in generate_all(record) if count_springs(r) == counts)
-    #print(r)
-    return r
-
-assert count_matching_combinations('???.###', [1,1,3]) == 1
-assert count_matching_combinations('.??..??...?##.', [1,1,3]) == 4
+        partial = '#'*first_count + '.'
+        min_rest_length = sum(rest_counts) + len(rest_counts) - 1
+        for i in range(length - min_rest_length):
+            prefix = '.' * i + partial
+            if not string_matches(record, prefix):
+                continue
+            rest_record = record[len(prefix):]
+            result += count_matching_strings(rest_record, rest_counts)
+    return result
 
 def solve1(data):
     rows = parse(data)
-    return sum(count_matching_combinations(r, c) for r, c in rows)
+    return sum(count_matching_strings(r, c) for r, c in rows)
 
 assert solve1(EXAMPLE) == 21
+
+def unfold_row(row):
+    record, counts = row
+    return simplify_record('?'.join(record for _ in range(5))), tuple(counts * 5)
+
+def solve2(data):
+    rows = [unfold_row(row) for row in parse(data)]
+    return sum(count_matching_strings(r, c) for r, c in rows)
+
+assert solve2(EXAMPLE) == 525152
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
