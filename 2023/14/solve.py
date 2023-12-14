fd2d7f830806681f5ba591b3ef32f063aeb03bcf
Knut Arild Erstad <knutae@gmail.com>
2023-12-14 10:49:14 +0100

    Day 14, part 2

--- 2023/14/solve.py
+++ 2023/14/solve.py
@@ -24,14 +24,37 @@ def parse(data):
     grid_size = len(grid[0]), len(grid)
     return grid_size, walls, rocks
 
-def tilt_north(walls, rocks):
+def move(rock, direction):
+    x,y = rock
+    dx,dy = direction
+    return x+dx,y+dy
+
+def inside_grid(grid_size, rock):
+    x, y = rock
+    w, h = grid_size
+    return x >= 0 and x < w and y >= 0 and y < h
+
+def sort_key(direction):
+    dx,dy = direction
+    if dx == 0:
+        return lambda r:-r[1]*dy
+    else:
+        return lambda r:-r[0]*dx
+
+def tilt(grid_size, walls, rocks, direction):
     rocks = set(rocks)
-    for rock in sorted(rocks, key=lambda r:r[1]):
+    for rock in sorted(rocks, key=sort_key(direction)):
         rocks.remove(rock)
-        x,y = rock
-        while y > 0 and (x,y-1) not in walls and (x,y-1) not in rocks:
-            y -= 1
-        rocks.add((x,y))
+        new_pos = move(rock, direction)
+        while inside_grid(grid_size, new_pos) and new_pos not in walls and new_pos not in rocks:
+            rock = new_pos
+            new_pos = move(rock, direction)
+        rocks.add(rock)
+    return rocks
+
+def cycle(grid_size, walls, rocks):
+    for direction in [(0,-1), (-1, 0), (0,1), (1, 0)]:
+        rocks = tilt(grid_size, walls, rocks, direction)
     return rocks
 
 def calculate_load(grid_size, rocks):
@@ -40,12 +63,36 @@ def calculate_load(grid_size, rocks):
 
 def solve1(data):
     grid_size, walls, rocks = parse(data)
-    rocks = tilt_north(walls, rocks)
+    rocks = tilt(grid_size, walls, rocks, (0, -1))
+    return calculate_load(grid_size, rocks)
+
+def print_board(grid_size, walls, rocks):
+    w, h = grid_size
+    for y in range(h):
+        print(''.join('O' if (x,y) in rocks else '#' if (x,y) in walls else '.' for x in range(w)))
+
+def solve2(data):
+    grid_size, walls, rocks = parse(data)
+    steps = []
+    seen = set()
+    while True:
+        rocks = frozenset(cycle(grid_size, walls, rocks))
+        if rocks in seen:
+            previous_step = steps.index(rocks)
+            cycle_length = len(steps) - previous_step
+            #print(f'step {len(steps)} cycle length {cycle_length} load {calculate_load(grid_size, rocks)}')
+            remaining = 1000000000 - 1 - len(steps)
+            if remaining % cycle_length == 0:
+                break
+        seen.add(rocks)
+        steps.append(rocks)
     return calculate_load(grid_size, rocks)
 
 assert solve1(EXAMPLE) == 136
+assert solve2(EXAMPLE) == 64
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
