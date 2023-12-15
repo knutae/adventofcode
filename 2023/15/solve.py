333b725411f3b7acbf1a54c7218c2b2735c9d976
Knut Arild Erstad <knutae@gmail.com>
2023-12-15 10:22:37 +0100

    Day 15, part 2

--- 2023/15/solve.py
+++ 2023/15/solve.py
@@ -14,7 +14,39 @@ def solve1(data):
 
 assert solve1(EXAMPLE) == 1320
 
+def solve2(data):
+    steps = data.strip().split(',')
+    boxes = [list() for _ in range(256)]
+    for step in steps:
+        if '=' in step:
+            label, n = step.split('=')
+            n = int(n)
+            box = boxes[HASH(label)]
+            existing = [i for i, x in enumerate(box) if x[0] == label]
+            if existing:
+                # replace existing
+                assert len(existing) == 1
+                box[existing[0]] = (label, n)
+            else:
+                # append
+                box.append((label, n))
+        else:
+            assert step.endswith('-')
+            label = step[:-1]
+            h = HASH(label)
+            boxes[h] = [(a, n) for a,n in boxes[h] if a != label]
+    result = 0
+    for i, box in enumerate(boxes):
+        #if box:
+        #    print(i, box)
+        for j, lens in enumerate(box):
+            result += (i+1) * (j+1) * lens[1]
+    return result
+
+assert solve2(EXAMPLE) == 145
+
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
