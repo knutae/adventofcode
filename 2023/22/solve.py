395a11bbf20cc480b51a1ba80574bba39bc5b779
Knut Arild Erstad <knutae@gmail.com>
2023-12-22 15:34:44 +0100

    Day 22, part 2

--- 2023/22/solve.py
+++ 2023/22/solve.py
@@ -47,8 +47,10 @@ def drop_brick(tower, brick):
         return move_to_height(brick, 1)
     current_z = lower_brick_z(brick)
     height = max(z for _,_,z in tower)
+    # use brick_coords to handle old brick being part of tower without mutating it
+    brick_coords = set(brick_coordinates(brick))
     for z in range(min(height, current_z-1),0,-1):
-        if any(p in tower for p in brick_coordinates(move_to_height(brick, z))):
+        if any(p not in brick_coords and p in tower for p in brick_coordinates(move_to_height(brick, z))):
             return move_to_height(brick, z+1)
     return move_to_height(brick, 1)
 
@@ -60,8 +62,7 @@ def can_disintegrate(bricks, brick, tower):
         if lower_brick_z(other_brick) != upper_z + 1:
             # only check bricks that could be touching the removed brick in the z direction
             continue
-        shakier_tower = shaky_tower - set(brick_coordinates(other_brick))
-        if drop_brick(shakier_tower, other_brick) != other_brick:
+        if drop_brick(shaky_tower, other_brick) != other_brick:
             return False
     return True
 
@@ -76,7 +77,35 @@ def solve1(data):
 
 assert solve1(EXAMPLE) == 5
 
+def number_of_falling_bricks(bricks, brick, tower):
+    shaky_tower = tower - set(brick_coordinates(brick))
+    upper_z = upper_brick_z(brick)
+    count = 0
+    higher_bricks = [b for b in bricks if lower_brick_z(b) > upper_z]
+    for other_brick in higher_bricks:
+        if brick == other_brick or lower_brick_z(other_brick) <= upper_z:
+            # can safely skip bricks lower than the brick to remove
+            continue
+        moved_brick = drop_brick(shaky_tower, other_brick);
+        if moved_brick != other_brick:
+            shaky_tower.difference_update(brick_coordinates(other_brick))
+            shaky_tower.update(brick_coordinates(moved_brick))
+            count += 1
+    return count
+
+def solve2(data):
+    bricks = parse(data)
+    tower = set()
+    for i, brick in enumerate(bricks):
+        brick = drop_brick(tower, brick)
+        bricks[i] = brick
+        tower.update(brick_coordinates(brick))
+    return sum(number_of_falling_bricks(bricks, brick, tower) for brick in bricks)
+
+assert solve2(EXAMPLE) == 7
+
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
