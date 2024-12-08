c53c3c116ef5d83301e5b7874521b6c8147b23c7
Knut Arild Erstad <knutae@gmail.com>
2024-12-08 18:28:45 +0100

    Day 8, part 2

--- 2024/08/solve.py
+++ 2024/08/solve.py
@@ -27,10 +27,13 @@ def parse(data):
                 antennas[c].add((x,y))
     return (width, height), dict(antennas)
 
-def checked_add(size, antinodes, node):
+def in_bounds(size, node):
     w, h = size
     x, y = node
-    if x in range(w) and y in range(h):
+    return x in range(w) and y in range(h)
+
+def checked_add(size, antinodes, node):
+    if in_bounds(size, node):
         antinodes.add(node)
 
 def solve1(data):
@@ -46,9 +49,34 @@ def solve1(data):
             checked_add(size, antinodes, (qx - dx, qy - dy))
     return len(antinodes)
 
+def solve2(data):
+    size, antennas = parse(data)
+    antinodes = set()
+    for antennas_list in antennas.values():
+        for p, q in combinations(antennas_list, 2):
+            px, py = p
+            qx, qy = q
+            dx = px - qx
+            dy = py - qy
+            # pass 1
+            x, y = p
+            while in_bounds(size, (x, y)):
+                antinodes.add((x, y))
+                x += dx
+                y += dy
+            # pass 2
+            x, y = p
+            while in_bounds(size, (x, y)):
+                antinodes.add((x, y))
+                x -= dx
+                y -= dy
+    return len(antinodes)
+
 assert solve1(EXAMPLE) == 14
+assert solve2(EXAMPLE) == 34
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
