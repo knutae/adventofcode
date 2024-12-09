5a97303d633d28a6b26efc8c209cebd3668f0bcc
Knut Arild Erstad <knutae@gmail.com>
2024-12-09 09:28:01 +0100

    Day 9, part 2 (not optimized)

--- 2024/09/solve.py
+++ 2024/09/solve.py
@@ -23,19 +23,51 @@ def defrag(blocks):
         while blocks[b] is None:
             b -= 1
         if a >= b:
-            return blocks[:a]
+            return blocks
         blocks[a] = blocks[b]
         blocks[b] = None
 
 def checksum(blocks):
-    return sum(i * n for i, n in enumerate(blocks))
+    return sum(i * n for i, n in enumerate(blocks) if n is not None)
 
 def solve1(data):
     return checksum(defrag(expand_blocks(parse(data))))
 
+def find_free_space(blocks, n, end):
+    free_count = 0
+    for i in range(end):
+        if blocks[i] is None:
+            free_count += 1
+            if free_count == n:
+                return i - n + 1
+        else:
+            free_count = 0
+    return None
+
+def defrag_files(blocks):
+    blocks = list(blocks)
+    max_id = max(n for n in blocks if n is not None)
+    for id in range(max_id, -1, -1):
+        # detecting the index and file size could be much faster...
+        file_index = blocks.index(id)
+        file_size = blocks.count(id)
+        free_index = find_free_space(blocks, file_size, file_index)
+        if free_index is not None:
+            for i in range(file_size):
+                assert blocks[file_index + i] == id
+                assert blocks[free_index + i] == None
+                blocks[free_index + i] = id
+                blocks[file_index + i] = None
+    return blocks
+
+def solve2(data):
+    return checksum(defrag_files(expand_blocks(parse(data))))
+
 assert solve1(EXAMPLE) == 1928
+assert solve2(EXAMPLE) == 2858
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
