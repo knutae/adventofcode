7aec8c093ca674f49d646fb5fed786ca4a79126b
Knut Arild Erstad <knutae@gmail.com>
2024-12-11 08:56:31 +0100

    Day 11, part 2

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 EXAMPLE = '125 17'
 
 def parse(data):
@@ -26,7 +28,33 @@ def solve1(data, steps=25):
 
 assert solve1(EXAMPLE) == 55312
 
+def blink2(stone_counts):
+    r = defaultdict(int)
+    for s, c in stone_counts.items():
+        ss = str(s)
+        if s == 0:
+            r[1] += c
+        elif len(ss) % 2 == 0:
+            split = len(ss) // 2
+            r[int(ss[:split])] += c
+            r[int(ss[split:])] += c
+        else:
+            r[s * 2024] += c
+    return r
+
+def solve2(data, steps=75):
+    stones = parse(data)
+    stone_counts = defaultdict(int)
+    for s in stones:
+        stone_counts[s] += 1
+    for _ in range(steps):
+        stone_counts = blink2(stone_counts)
+    return sum(stone_counts.values())
+
+assert solve2(EXAMPLE, steps=25) == 55312
+
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
