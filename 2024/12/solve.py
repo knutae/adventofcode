bb902a90e17923848e00216d30c58f791ca15066
Knut Arild Erstad <knutae@gmail.com>
2024-12-12 11:38:47 +0100

    Day 12, part 2

--- 2024/12/solve.py
+++ 2024/12/solve.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 SMALL_EXAMPLE = '''
 AAAA
 BBCD
@@ -75,10 +77,53 @@ def solve1(data):
         total_price += len(r) * perimiter_length(r)
     return total_price
 
+def perimiter_sides(region):
+    left_fences = defaultdict(set)
+    right_fences = defaultdict(set)
+    up_fences = defaultdict(set)
+    down_fences = defaultdict(set)
+    for p in region:
+        px, py = p
+        for q in adjacent(p):
+            if q in region:
+                continue
+            qx, qy = q
+            x = min(px, qx)
+            y = min(py, qy)
+            if py == qy:
+                fences = left_fences if qx < px else right_fences
+                assert y not in fences[x]
+                fences[x].add(y)
+            else:
+                assert px == qx
+                fences = up_fences if qy < py else down_fences
+                assert x not in fences[y]
+                fences[y].add(x)
+    sides = 0
+    for fences in (left_fences, right_fences, up_fences, down_fences):
+        for pieces in fences.values():
+            prev = None
+            for n in sorted(pieces):
+                if n-1 != prev:
+                    sides += 1
+                prev = n
+    return sides
+
+def solve2(data):
+    plants = parse(data)
+    regions = all_regions(plants)
+    total_price = 0
+    for r in regions:
+        total_price += len(r) * perimiter_sides(r)
+    return total_price
+
 assert solve1(SMALL_EXAMPLE) == 140
 assert solve1(EXAMPLE) == 1930
+assert solve2(SMALL_EXAMPLE) == 80
+assert solve2(EXAMPLE) == 1206
 
 with open('input') as f:
     data = f.read()
 
 print(solve1(data))
+print(solve2(data))
