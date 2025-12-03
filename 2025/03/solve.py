9732f3c45e885fa8dbe87d5d763ed2ed581fdb9e
Knut Arild Erstad <knutae@gmail.com>
2025-12-03 13:04:43 +0100

    Day 3 part 2

--- 2025/03/solve.py
+++ 2025/03/solve.py
@@ -11,17 +11,31 @@ def parse(s):
         for line in s.strip().split('\n')
     ]
 
-def highest_joltage(bank):
-    first = max(bank[:-1])
-    second = max(bank[bank.index(first)+1:])
-    return 10*first + second
+def highest_joltage(bank, n):
+    result = []
+    for i in range(n):
+        if i == n-1:
+            # last
+            c = max(bank)
+        else:
+            c = max(bank[:-n + i + 1])
+            index = bank.index(c)
+            bank = bank[index+1:]
+        result.append(c)
+    return int(''.join(str(c) for c in result))
 
 def solve1(s):
-    return sum(highest_joltage(bank) for bank in parse(s))
+    return sum(highest_joltage(bank, 2) for bank in parse(s))
 
 assert solve1(EXAMPLE) == 357
+
+def solve2(s):
+    return sum(highest_joltage(bank, 12) for bank in parse(s))
+
+assert solve2(EXAMPLE) == 3121910778619
 
 with open('input') as f:
     s = f.read()
 
 print(solve1(s))
+print(solve2(s))
