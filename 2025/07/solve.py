f655c2f8a87889789a9a3617b92dc39b5b650214
Knut Arild Erstad <knutae@gmail.com>
2025-12-07 07:58:06 +0100

    Day 7 part 2

--- 2025/07/solve.py
+++ 2025/07/solve.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 EXAMPLE = '''
 .......S.......
 ...............
@@ -40,7 +42,24 @@ def solve1(s):
 
 assert solve1(EXAMPLE) == 21
 
+def solve2(s):
+    beam, lines = parse(s)
+    timelines = {beam: 1}
+    for line in lines:
+        new_timelines = defaultdict(int)
+        for beam, count in timelines.items():
+            if line[beam] == '^':
+                new_timelines[beam-1] += count
+                new_timelines[beam+1] += count
+            else:
+                new_timelines[beam] += count
+        timelines = new_timelines
+    return sum(timelines.values())
+
+assert solve2(EXAMPLE) == 40
+
 with open('input') as f:
     s = f.read()
 
 print(solve1(s))
+print(solve2(s))
