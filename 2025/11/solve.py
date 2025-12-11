7ce585e0bd9b1c1394e060d55f3e681a597d3bbe
Knut Arild Erstad <knutae@gmail.com>
2025-12-11 16:25:41 -0500

    Day 11 part 2

--- 2025/11/solve.py
+++ 2025/11/solve.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 EXAMPLE = '''
 aaa: you hhh
 you: bbb ccc
@@ -33,7 +35,48 @@ def solve1(s):
 
 assert solve1(EXAMPLE) == 5
 
+EXAMPLE2 = '''
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+'''
+
+class Solver:
+    def __init__(self, graph):
+        self.graph = graph
+
+    @cache
+    def count_dac_fft_paths(self, node, dac, fft):
+        if node == 'out':
+            if dac and fft:
+                return 1
+            else:
+                return 0
+        r = 0
+        for next in self.graph[node]:
+            assert not (dac and next == 'dac')
+            assert not (fft and next == 'fft')
+            r += self.count_dac_fft_paths(next, dac or next == 'dac', fft or next == 'fft')
+        return r
+
+def solve2(s):
+    solver = Solver(parse(s))
+    return solver.count_dac_fft_paths('svr', False, False)
+
+assert solve2(EXAMPLE2) == 2
+
 with open('input') as f:
     s = f.read()
 
 print(solve1(s))
+print(solve2(s))
